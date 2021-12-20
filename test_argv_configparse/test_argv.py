70b569f37c911a7073ff9f428b587550c001ca07
luping.xiao <xiaoluping@yuanfudao.com>
2021-12-20 14:52:18 +0800

    argv[n]使用

---
+++ test_argv_configparse/test_argv.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# @Time : 2021/12/17 3:01 下午
+# @Author : xiaoluping
+# @Email : xiaoluping@yuanfudao.com
+# @File : test_argv.py
+
+import sys
+
+a = sys.argv[2:]
+print(a)
